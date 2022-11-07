84d15460229a3e604326880411c1506f04de4443
Kirill N <nklv65@gmail.com>
2022-11-07 13:21:05 +0500

    step 2

--- lessons.py
+++ lessons.py
@@ -1,14 +1,10 @@
-x = [1, 2]
+import os
+import time
 
-if x == 0:
-    x = 1
-    print('x был равен нулю')
+os.system('start ' + 'https://www.youtube.com/')
 
-elif type(x) == type(5) or type(x) == type(5.5):
-    print('x допустимое значение')
+time.sleep(5)
 
-else:
-    print('В x не допустимый тип данных')
-    x = 1
+os.startfile('C:\Program Files\Git\git-bash')
 
-print(100/x)
+#this code open youtube and start of git-bash
