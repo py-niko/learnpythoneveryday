d0a262bdd1fd2c9ec1fcbb35f4b9943290938bd7
Kirill N <nklv65@gmail.com>
2022-11-07 13:56:46 +0500

    step 2

--- lessons.py
+++ lessons.py
@@ -1,10 +1,19 @@
 import os
-import time
 
-os.system('start ' + 'https://www.youtube.com/')
+while True:
+    sait = input('Введите название сайта\n')
 
-time.sleep(5)
+    if sait == 'Завершить':
+        break
 
-os.startfile('C:\Program Files\Git\git-bash')
-
-#this code open youtube and start of git-bash
+    if 'https://' in sait:
+        os.system('start ' + sait)
+        print('if')
+    elif 'www.' in sait:
+        sait = 'https://' + sait
+        os.system('start ' + sait)
+        print('elif')
+    else:
+        sait = 'https://www' + '.' + sait
+        os.system('start ' + sait)
+        print('else')
