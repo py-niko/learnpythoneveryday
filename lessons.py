79f4b33f8cff66c0eaafe4ceefb3d9c2d704f945
Kirill N <nklv65@gmail.com>
2022-11-07 13:05:30 +0500

    Step 1

---
+++ lessons.py
@@ -0,0 +1,14 @@
+x = [1, 2]
+
+if x == 0:
+    x = 1
+    print('x был равен нулю')
+
+elif type(x) == type(5) or type(x) == type(5.5):
+    print('x допустимое значение')
+
+else:
+    print('В x не допустимый тип данных')
+    x = 1
+
+print(100/x)
